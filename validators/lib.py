e14f507f6356202dd152960f622bafd137b9b6a2
PurpleSingularity <44556348+PurpleSingularity@users.noreply.github.com>
2018-11-30 16:09:18 +0200

    Add files via upload

--- validators/lib.py
+++ validators/lib.py
@@ -2,15 +2,16 @@
 """
     Написати валідатор ....
     Правило валідації
+    xx-444-1
 """
 
 import re
 
-def getUserEmail():
+def getUserPasCode():
 
-    user_input = input(?)
+    user_input = input('')
 
-    if (re.match(r"?", user_input) ):
+    if (re.match(r"(\W{2}),(-),(\d{3}),(-),(\d{1})", user_input) ):
         return user_input
     else:
         return False
@@ -19,18 +20,30 @@ def getUserEmail():
 """
     Написати валідатор ....
     Правило валідації
+    Слова, одне або декілька, з велкої літери
 """
 
-def getProductName():
-    #TODO
+def getCountryName():
+    user_input = input('')
+
+    if (re.search(r"/w+", user_input) ):
+        return user_input
+    else:
+        return False
 
 
 
 """
     Написати валідатор ....
     Правило валідації
+    1111.11' ''usd'
 """
 
 
-def getProductPrice():
-    #TODO
+def getMoney():
+    user_input = input('')
+
+    if (re.match(r"(/d{1-4},/.,/d{1-2}), ,usd", user_input) ):
+        return user_input
+    else:
+        return False
